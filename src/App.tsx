ebd98869d3650dfff53a60a1050e246ff268e4d2
Manoel Valladao <manoel.valladao.partner@decathlon.com>
2024-09-14 18:23:29 -0300

    Make it work

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,39 @@
-import './App.css'
+// Import Swiper React components
+import {Swiper, SwiperSlide} from 'swiper/react';
 
-// core version + navigation, pagination modules:
-import Swiper from 'swiper';
-import {Navigation, Pagination} from 'swiper/modules';
-
-// import Swiper and modules styles
+// Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
+import './App.css'
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+// import required modules
+import {FreeMode, Pagination} from 'swiper/modules';
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-function App() {
+export default function App() {
   return (
     <>
-      <div className="swiper">
-        <div className="swiper-wrapper">
-          <div className="swiper-slide">Slide 1</div>
-          <div className="swiper-slide">Slide 2</div>
-          <div className="swiper-slide">Slide 3</div>
-        </div>
-        <div className="swiper-pagination"></div>
-
-        <div className="swiper-button-prev"></div>
-        <div className="swiper-button-next"></div>
-
-        <div className="swiper-scrollbar"></div>
-      </div>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </>
-  )
+  );
 }
-
-export default App
