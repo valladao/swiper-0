5d008411c1a53d21cc5d3c8db02ab51e034686ac
Manoel Valladao <manoel.valladao.partner@decathlon.com>
2024-09-14 23:18:08 -0300

    Add photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,43 @@
 // Import Swiper React components
 import {Swiper, SwiperSlide} from 'swiper/react';
+import photo1 from '../public/photo-1.jpg'
+import photo2 from '../public/photo-2.jpg'
+import photo3 from '../public/photo-3.jpg'
+import photo4 from '../public/photo-4.jpg'
+
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
 
 import './App.css'
 
 // import required modules
-import {FreeMode, Pagination} from 'swiper/modules';
+import {FreeMode} from 'swiper/modules';
 
 export default function App() {
   return (
     <>
       <Swiper
         slidesPerView={3}
-        spaceBetween={30}
+        spaceBetween={0}
         freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
+        loop={true}
+        modules={[FreeMode]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <img src={photo1} alt="Photo1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={photo2} alt="Photo2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={photo3} alt="Photo3" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={photo4} alt="Photo4" />
+        </SwiperSlide>
       </Swiper>
     </>
   );
